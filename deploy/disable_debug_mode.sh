f3a8a4e520d87848b805780b9ceab43d27a53f66
Sergey Lemnev <lemnev@realweb.ru>
2013-04-13 12:49:43 +0400

    add savely to csrf

---
+++ deploy/disable_debug_mode.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd $WORKSPACE/app
+sed -i -e "s/defined('YII_DEBUG') or define('YII_DEBUG',true);/defined('YII_DEBUG') or define('YII_DEBUG',false);/g" index.php
+sed -i -e "s/defined('YII_DEBUG') or define('YII_DEBUG',true);/defined('YII_DEBUG') or define('YII_DEBUG',false);/g" protected/yiic.php
+sed -i -e "s/defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);//g" index.php
+sed -i -e "s/defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);//g" protected/yiic.php
